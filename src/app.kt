be76540f270dcc44cb237286e7d4aa763b4fa56b
dohieu19999@gmail.com <Dobatrang03031966>
2021-03-16 18:12:10 +0700

    Initial commit

---
+++ src/app.kt
@@ -0,0 +1,46 @@
+/**
+ * @author Trần Duy Thanh
+ * @param x biến x
+ * @param y biến y
+ * @return tổng của x và y
+ * @sample x=5, y=6 => 11
+ * Đây là ghi chú docs
+ */
+fun Cong(x:Int,y:Int):Int
+{
+    return x+y
+}/*
+Đây là hàm main
+dùng để chạy chương trình
+*/
+fun main() {
+    println("Hello World!")
+    println("Obama")
+    println("Putin")
+    println("Kim Jong Un")
+    //gọi hàm Cộng 2 số
+    val t:Int=Cong(7,8)
+    println(t)
+//    Kiểu dữ liệu trong kotlin
+    var x:Long=100L
+    var y:Double=110.0
+    var z:Byte=1
+    var x2:Short= 3
+    var i:Int = 333
+
+    var address:String=""""
+số 24 đường 7, khu phố X
+phường 5, Quận 9
+yhtry'rty
+try
+rty
+rtyr
+tỷ
+tỷ
+tyu
+tru
+rt
+TP.HCM
+""""
+print(address)
+}
